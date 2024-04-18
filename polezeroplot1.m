f6f6a2f8a398d69c3c5caa92e72c047715aec82e
Ian Eykamp <ian@eykamp.com>
2024-04-18 01:20:44 -0400

    add root locus plots for PD controller

---
+++ polezeroplot1.m
@@ -0,0 +1,20 @@
+b = 1.04e-2; % N * s / mm
+m = 6.00e-3; % g
+a = 4.06e-0; % N / mm
+
+Fm0 = 7.01e-02; % N / A
+
+Kp = 1; % parameter to vary
+Kd = 1 ./ 20;
+
+C_s = tf([Kd, Kp], [1]);
+G_s = tf([Fm0], [m, b, -a]);
+
+G2_s = C_s * G_s
+
+figure(1);
+clf;
+rlocus(G_s)
+figure(2);
+clf;
+rlocus(G_s, G2_s)
