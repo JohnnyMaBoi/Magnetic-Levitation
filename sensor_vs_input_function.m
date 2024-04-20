107c57e22614a0005ef5e0f10713892ea29dd79a
Rajiv Perera <37748104+JohnnyMaBoi@users.noreply.github.com>
2024-04-19 23:24:36 -0400

    Calculated teslas from sensor voltage and compared to magnet current

--- sensor_vs_input_function.m
+++ sensor_vs_input_function.m
@@ -18,5 +18,42 @@ range_4 = logical(full_range);
 range_4(sweep_len * 5 + 1:end) = true;
 % plot(analog_write(range_1), sensor_reading(range_1));
 plot(analog_write(range_2), sensor_reading(range_2));
+
+% sensor reading is 
+sensor_voltage = (sensorvsinput.SensorReading).*(3.3/1023);
+
+%terms from datasheet page 10, sens from table
+Vq = 3.3/2;
+sensitivity_term = 7.5; % changes by device & voltage
+temp_effect = sensitivity_term*(1+(22-25)*.0012); %assuming 22 deg celsius 
+% solve for magnetic flux b
+sensor_flux = (sensor_voltage - Vq)/(temp_effect);
+figure()
+%values for plotting SOLENOID SCALING ARBITRARY
+time = 1:size(sensorvsinput.AnalogWriteValue);
+solenoid_scale = .1/1024;
+hold on;
+plot(time, sensor_flux, Color='r')
+plot(time, analog_write*solenoid_scale, Color='b')
+xlabel("Sensor readings over time")
+legend("Sensed flux in Teslas", "INCORRECT SCALE solenoid current")
+title("Sensor Flux in Teslas")
+
+% From reading the motor board page, it seems it has "magic PWM" support
+% which sounds suspicious. I suspect that the noise we are seeing
+% is from this PWM. Moving-mean should show what the average magnetic
+% flux is, but this might indicate we will need to do some 
+% averaging on the sensing end of our arduino code as well 
+% chose 40 as its width of 0-ish sections
+figure()
+pwm_adjusted_sensor = movmean(sensor_flux, 40);
+hold on;
+plot(time, pwm_adjusted_sensor, Color='r')
+plot(time, analog_write*solenoid_scale, Color='b')
+xlabel("Sensor readings over time")
+legend("PWM-removed Sensed flux in Teslas", "INCORRECT SCALE solenoid current")
+title("PWM-removed flux over time")
+
+
 % plot(analog_write(range_3), sensor_reading(range_3));
 % plot(analog_write(range_4), sensor_reading(range_4));
