ea3291011e14b23e32ab9e9bd91d9cba888f74fa
Ian Eykamp <ian@eykamp.com>
2024-04-22 13:39:11 -0400

    hall effect sensor analysis

--- magnetic_levitation/src/main-controller01.cpp
+++ magnetic_levitation/src/main-controller01.cpp
@@ -25,6 +25,8 @@ int hall;  // analog output variable
 unsigned long startMillis;  // some global variables available anywhere in the program
 unsigned long currentMillis;
 
+int between(int lower, int upper, int val);
+
 float hall_mT(int hall_analog) {
     // % solve for magnetic flux in units of mT
     // voltages are all in units of mV
@@ -33,8 +35,11 @@ float hall_mT(int hall_analog) {
     return sensor_flux;
 }
 
-const unsigned int hall_mT_moving_average_size = 25;
+const unsigned int hall_mT_moving_average_size = 4;
 float hall_mT_moving_average_array[hall_mT_moving_average_size];
+float prev_moving_average_value = 0;
+float new_moving_average_value = 0;
+int controller_val;
 
 float moving_average_filter(float new_val, float* moving_average_array, unsigned int moving_average_len) {
     float moving_average = new_val;
@@ -61,8 +66,28 @@ void setup() {
     pinMode(MOTOR_IN2, OUTPUT);
     pinMode(SENSOR_PIN, INPUT);
     analogWrite(MOTOR_IN1, LOW);
-    analogWrite(MOTOR_IN2, 200);
     startMillis = millis();  // initial start time
+}
+
+int steady_state_output = 207;
+float steady_state_hall_effect = 34;
+float Kp = 5;
+float Kd = Kd / 30;
+int controller(float hall_mT, float prev_hall_mT) {
+    float hall_difference = hall_mT - steady_state_hall_effect;
+    float dhalldt = hall_mT - prev_hall_mT;
+    return between(50, 250, steady_state_output - int(Kd * dhalldt) - int(Kp * hall_difference));
+    // return between(50, 250, steady_state_output - int(Kp * hall_difference));
+}
+
+int between(int lower, int upper, int val) {
+    if (val < lower) {
+        return lower;
+    }
+    if (val > upper) {
+        return upper;
+    }
+    return val;
 }
 
 void loop() {
@@ -71,15 +96,17 @@ void loop() {
     // read sensor analog value
     currentMillis = millis();
     // plot processed and unprocessed result
-    Serial.print(">hall_analog:");
-    Serial.print(String(currentMillis) + ":");
-    Serial.println(hall);
-
-    Serial.print(">hall_mT:");
-    Serial.print(String(currentMillis) + ":");
-    Serial.println(hall_mT(hall));
+    prev_moving_average_value = new_moving_average_value;
+    new_moving_average_value = moving_average_filter(hall_mT(hall), hall_mT_moving_average_array, hall_mT_moving_average_size);
+    controller_val = controller(new_moving_average_value, prev_moving_average_value);
 
     Serial.print(">hall_mT_filtered:");
     Serial.print(String(currentMillis) + ":");
-    Serial.println(moving_average_filter(hall, hall_mT_moving_average_array, hall_mT_moving_average_size));
+    Serial.println(new_moving_average_value);
+
+    Serial.print(">controller val:");
+    Serial.print(String(currentMillis) + ":");
+    Serial.println(controller_val);
+
+    analogWrite(MOTOR_IN2, controller_val);
 }
---
+++ sensor_data_analysis/filter_hall_sensor01.m
@@ -0,0 +1,102 @@
+load('raw_mT_01.mat');
+
+%% plot raw data
+figure(1);
+clf;
+hold on;
+plot(hall_mT.timestampms, hall_mT.hall_mT, 'b-');
+xlabel("Timestamp (ms)");
+ylabel("Hall effect sensor (mT)");
+title("Unfiltered data");
+
+%% plot raw data
+figure(2);
+clf;
+hold on;
+plot(hall_mT.timestampms, hall_mT.hall_mT, 'b-');
+plot(hall_mT.timestampms, movmean(hall_mT.hall_mT, [25, 0]), 'k-');
+xlabel("Timestamp (ms)");
+ylabel("Hall effect sensor (mT)");
+title("25-point moving average");
+
+%% peak detectors v1
+% softmax([x1, x2, ..., xn]) = log(sum(exp([x1, x2, ..., xn]))
+% moving softmax = log(
+filter_len = 3;
+buffer_factor = -0.2;
+moving_softmax_filter = ones(1, filter_len);
+peak_detector_top = log(conv(exp(hall_mT.hall_mT), moving_softmax_filter, 'same') ./ filter_len);
+peak_detector_bottom = -log(conv(exp(-hall_mT.hall_mT), moving_softmax_filter, 'same') ./ filter_len);
+peak_detector_diff = peak_detector_top - peak_detector_bottom;
+peak_detector_top = peak_detector_top + peak_detector_diff * buffer_factor;
+peak_detector_bottom = peak_detector_bottom - peak_detector_diff * buffer_factor;
+figure(3);
+clf;
+hold on;
+plot(hall_mT.timestampms, hall_mT.hall_mT, 'b-');
+plot(hall_mT.timestampms, peak_detector_top, 'r-');
+plot(hall_mT.timestampms, peak_detector_bottom, 'g-');
+xlabel("Timestamp (ms)");
+ylabel("Hall effect sensor (mT)");
+title("Softmax peak detectors");
+
+%% peak detectors v2
+moving_softmax_filter = ones(1, filter_len);
+peak_detector_top = movmax(hall_mT.hall_mT, [filter_len, 0]);
+peak_detector_bottom = movmin(hall_mT.hall_mT, [filter_len, 0]);
+peak_detector_diff = peak_detector_top - peak_detector_bottom;
+peak_detector_top = peak_detector_top + peak_detector_diff * buffer_factor;
+peak_detector_bottom = peak_detector_bottom - peak_detector_diff * buffer_factor;
+figure(4);
+clf;
+hold on;
+plot(hall_mT.timestampms, hall_mT.hall_mT, 'b-');
+plot(hall_mT.timestampms, peak_detector_top, 'r-');
+plot(hall_mT.timestampms, peak_detector_bottom, 'g-');
+
+%% bouncer average
+filter_len = 3;
+buffer_factor = -0.5;
+
+filtered_mT = zeros(size(hall_mT.hall_mT));
+filtered_mT(1:filter_len) = hall_mT.hall_mT(1);
+max_vals = zeros(size(hall_mT.hall_mT));
+min_vals = zeros(size(hall_mT.hall_mT));
+max_vals(1:filter_len) = hall_mT.hall_mT(1);
+min_vals(1:filter_len) = hall_mT.hall_mT(1);
+
+for idx = 2:length(hall_mT.hall_mT)
+    moving_max_len = min(idx - 1, filter_len);
+    moving_max_value = max(hall_mT.hall_mT(idx - moving_max_len:idx - 1));
+    moving_min_value = min(hall_mT.hall_mT(idx - moving_max_len:idx - 1));
+    max_vals(idx) = moving_max_value;
+    min_vals(idx) = moving_min_value;
+
+    if filtered_mT(idx - 1) < moving_min_value || filtered_mT(idx - 1) > moving_max_value
+        filtered_mT(idx) = (moving_min_value + moving_max_value) ./ 2;
+    else
+        filtered_mT(idx) = filtered_mT(idx - 1);
+    end
+end
+figure(5);
+clf;
+hold on;
+plot(hall_mT.timestampms, hall_mT.hall_mT, 'b-');
+plot(hall_mT.timestampms, max_vals, 'r-');
+plot(hall_mT.timestampms, min_vals, 'g-');
+plot(hall_mT.timestampms, filtered_mT, 'k-');
+xlabel("Timestamp (ms)");
+ylabel("Hall effect sensor (mT)");
+title("Moving max peak detectors");
+
+%% derivative signal
+figure(6);
+clf;
+hold on;
+plot(hall_mT.timestampms, hall_mT.hall_mT, 'b-');
+plot(hall_mT.timestampms, filtered_mT, 'k-');
+plot(hall_mT.timestampms, movmean(filtered_mT, [2, 0]), 'g.-');
+plot(hall_mT.timestampms, movmean(hall_mT.hall_mT, [3, 0]), 'r.-');
+xlabel("Timestamp (ms)");
+ylabel("Hall effect sensor (mT)");
+title("Moving max peak detectors plus low pass filter");
